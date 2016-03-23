b3f3a6db2c4184553d22d37f9142ad7048229410
mattsteinpreis <matt@nexosis.com>
2016-03-23 17:07:05 -0400

    added

---
+++ GameOf2048.py
@@ -0,0 +1,13 @@
+__author__ = 'matt'
+
+"""
+Code for CodeAbbey #91: Game of 2048
+
+The problem gives a board set-up and a list of moves,
+and asks for the number of each 2, 4, etc.
+"""
+
+
+class Board(object):
+    def __init__(self):
+        pass
