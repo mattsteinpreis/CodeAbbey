185aed774114bc91f9acc3b34b2b70ab11c28072
mattsteinpreis <matt@nexosis.com>
2016-03-24 17:31:18 -0400

    defined first set of functions

--- GameOf2048.py
+++ GameOf2048.py
@@ -10,4 +10,47 @@ and asks for the number of each 2, 4, etc.
 
 class Board(object):
     def __init__(self):
-        pass
+        self.tiles = None
+        self.move_inv = {'R':'R', 'L':'L', 'U':'D', 'D':'U'}
+
+    def to_string(self):
+        return ''.join([ch for row in self.tiles for ch in row])
+
+    def fill(self, s):
+        m = []
+        for i in range(4):
+            m.append([ch for ch in s[i*4:((i+1)*4)]])
+        self.tiles = m
+
+    def show(self):
+        for row in self.tiles:
+            print ' '.join(row)
+
+    def rotate(self, d):
+        s = self.to_string()
+        if d == 'L':
+            t = s[3::-1]+s[7:3:-1]+s[11:7:-1]+s[15:11:-1]
+        self.fill(t)
+
+    def move(self, d):
+        self.rotate(d)
+        #self.condense()
+        self.rotate(self.move_inv[d])
+
+
+def test():
+    start = '2222424422244442'
+    b = Board()
+    b.fill(start)
+    b.show()
+    b1 = b.to_string()
+    b.move('L')
+    b2 = b.to_string()
+
+
+def CodeAbbey():
+    pass
+
+
+if __name__ == '__main__':
+    test()
